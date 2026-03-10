4d17f0491ce109f627910ff52ecd08288b007e7e
Enric Presti <165492761+enerico123@users.noreply.github.com>
2026-03-10 10:20:04 +0100

    feat : frontend chat messages

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,9 +1,35 @@
-function Chat({ token, userId }) {
+import { useEffect, useState } from "react"
+
+const Chat = ({token, userId, conversationId}) => {
+
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    const chargerMessages = async () => {
+            const response = await fetch(`/api/messages/${conversationId}`, {
+            headers: { 
+                'Authorization': `Bearer ${token}` 
+            }
+            })
+            const data = await response.json()
+            setMessages(data)
+        }
+        chargerMessages()
+  },[conversationId])
+
   return (
-    <div>
-      <h1>Bienvenue ! 🎉</h1>
-      <p>UserId : {userId}</p>
-    </div>
+    <>
+    {/* liste des messages  */}
+    
+    {messages.map((mess) => (
+      <div key={mess.id}>
+        {mess.content}
+      </div>
+    ))}
+
+    {/* input pour ecrire un message  */}
+    <input type="text" />
+    </>
   )
 }
 
